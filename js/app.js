7f1ef48838feca6f521b92558332e0b522524f5f
Jelani R <20979096+Jchips@users.noreply.github.com>
2023-02-18 17:01:08 -0800

    changed the button to a toggle button, removed commented code

--- js/app.js
+++ js/app.js
@@ -2,55 +2,27 @@
 
 window.addEventListener('load', init);
 
-function init() {
-  let answerChoicesNo = answersNo();
-  console.log(answerChoicesNo[0]);
-  
-  let answerChoicesYes = answersYes();
-  console.log(answerChoicesYes[0]);
-
-  let userName = prompt('what\'s your name fam?');
-  alert('Hello ' + userName + '. Welcome.');
-
-  let correctAnswers = 0;
-
-  correctAnswers = guessingGame(correctAnswers, answerChoicesYes, answerChoicesNo);
-  correctAnswers = guessNum(correctAnswers);
-  correctAnswers = guessStates(correctAnswers);
-
-  alert('you got ' + correctAnswers + ' answers correct out of 7');
-
-  alert('ok ' + userName + '. That was fun. Read my bio to learn more about me');
-
-  let button = document.getElementById('btn');
-  button.addEventListener('click', displayAnswers);
-}
-
-
-
-
 // Runs the guessing game to play with the user
 function guessingGame(correctAnswers, answerChoicesYes, answerChoicesNo) {
   alert('let\'s play a guessing game.\nAnswer with \'yes\', \'y\', \'no\' or \'n\' for each question!');
   let question1 = document.getElementById('q1');
   let userResponse1 = prompt(question1.textContent);
-  
   correctAnswers = response(correctAnswers, userResponse1, answerChoicesYes);
+
   let question2 = document.getElementById('q2');
   let userResponse2 = prompt(question2.textContent);
-
   correctAnswers = response(correctAnswers, userResponse2, answerChoicesNo);
+
   let question3 = document.getElementById('q3');
   let userResponse3 = prompt(question3.textContent);
- 
   correctAnswers = response(correctAnswers, userResponse3, answerChoicesYes);
+
   let question4 = document.getElementById('q4');
   let userResponse4 = prompt(question4.textContent);
- 
   correctAnswers = response(correctAnswers, userResponse4, answerChoicesYes);
+
   let question5 = document.getElementById('q5');
   let userResponse5 = prompt(question5.textContent);
-   
   correctAnswers = response(correctAnswers, userResponse5, answerChoicesNo);
   return correctAnswers;
 }
@@ -72,15 +44,11 @@ function response(correctAnswers, userResponse, answerChoices) {
   userResponse = userResponse.toLowerCase();
   if (userResponse === answerChoices[0] || userResponse === answerChoices[1]) {
     alert('You are correct');
-    // console.log('you are correct');
     correctAnswers++;
-    console.log('correct: ' + correctAnswers);
   } else if (userResponse !== 'yes' && userResponse !== 'y' && userResponse !== 'no' && userResponse !== 'n') {
     alert('please respond with either \'yes\', \'y\', \'no\' or \'n\' next time');
-    // console.log('please respond with either \'yes\', \'y\', \'no\' or \'n\' next time');
   } else {
     alert('sorry, you are WRONG');
-    // console.log('sorry, you are WRONG');
   }
   return correctAnswers;
 }
@@ -88,23 +56,14 @@ function response(correctAnswers, userResponse, answerChoices) {
 // Displays the answers to my guessing game questions
 function displayAnswers() {
   let answers = document.getElementById('answers');
-  let orderedList = document.createElement('ol');
-  let li1 = document.createElement('li');
-  li1.innerHTML = 'yes, I love R&B';
-  let li2 = document.createElement('li');
-  li2.textContent = 'I only wish I could do a backflip...';
-  let li3 = document.createElement('li');
-  li3.textContent = 'yes, Samsung all day';
-  let li4 = document.createElement('li');
-  li4.textContent = 'yeah';
-  let li5 = document.createElement('li');
-  li5.textContent = 'I do not like ice cream';
-  orderedList.appendChild(li1);
-  orderedList.appendChild(li2);
-  orderedList.appendChild(li3);
-  orderedList.appendChild(li4);
-  orderedList.appendChild(li5);
-  answers.appendChild(orderedList);
+  let btn = document.getElementById('btn');
+  if (answers.style.display === 'none' || answers.style.display === '') {
+    answers.style.display = 'block';
+    btn.textContent = 'Click here to hide the answers!';
+  } else {
+    answers.style.display = 'none';
+    btn.textContent = 'Click here to see the answers!';
+  }
 }
 
 // has user guess how many siblings I have
@@ -120,7 +79,6 @@ function guessNum(correctAnswers) {
     } else if (num === siblingsAnswer) {
       alert('you guessed it!');
       correctAnswers++;
-      console.log('correct: ' + correctAnswers);
       break;
     } else {
       alert('please enter a number next time');
@@ -141,7 +99,6 @@ function guessStates(correctAnswers) {
       if (guess === states[j]) {
         alert('yeah, that\'s one of them');
         correctAnswers++;
-        console.log('correct: ' + correctAnswers);
         guessed = true;
         break;
       }
@@ -159,6 +116,27 @@ function guessStates(correctAnswers) {
   statesList += states[states.length - 1] + '.';
   alert(statesList);
   return correctAnswers;
+}
+
+function init() {
+  let userName = prompt('what\'s your name fam?');
+  alert('Hello ' + userName + '. Welcome.');
+
+  let correctAnswers = 0;
+
+  let answerChoicesNo = answersNo();
+  let answerChoicesYes = answersYes();
+
+  correctAnswers = guessingGame(correctAnswers, answerChoicesYes, answerChoicesNo);
+  correctAnswers = guessNum(correctAnswers);
+  correctAnswers = guessStates(correctAnswers);
+
+  alert('you got ' + correctAnswers + ' answers correct out of 7');
+
+  alert('ok ' + userName + '. That was fun. Read my bio to learn more about me');
+
+  let button = document.getElementById('btn');
+  button.addEventListener('click', displayAnswers);
 }
 
 
